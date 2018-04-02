27918a409270d0be203f6003936978958727633c
gehrig rankin <rgrankin22@gmail.com>
2018-04-02 09:52:13 -0700

    updated bio and portfolio

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+$("#psychic-game").click(function(){
+    $("#psychic-panel").slideToggle("slow");
+});
+
+$("#crystals-game").click(function(){
+    $("#crystals-panel").slideToggle("slow");
+});
+
+$("#trivia-game").click(function(){
+    $("#trivia-panel").slideToggle("slow");
+});
